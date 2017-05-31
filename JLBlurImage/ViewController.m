71efdcae1fd8dd3026c1eeabbb5d625f3fe98587
lipei007 <macmini1@mini.local>
2017-05-31 13:34:53 +0800

    …

--- JLBlurImage/ViewController.m
+++ JLBlurImage/ViewController.m
@@ -7,23 +7,240 @@
 //
 
 #import "ViewController.h"
+#import <Accelerate/Accelerate.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *imgView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+#pragma mark - View Controller LifeCyle
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    if (self) {
+        
+    }
+    return self;
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIImage *originImage = [UIImage imageNamed:@"Aktun.jpg"];
+    
+    // core iamge
+    UIImage *bluredImage = [self gaussBlurImage:originImage blurLevel:30];
+    self.imgView.image = bluredImage;
+    
+    // vImage
+//    UIImage *bluredImage = [self blurryImage:originImage withBlurLevel:0.9];
+//    self.imgView.image = bluredImage;
+//    
+    // visualEffect
+//    self.imgView.image = originImage;
+//    [self visualEffectView:self.imgView];
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
+
+- (void)dealloc {
+    
+}
+
+#pragma mark - Rotation
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    return YES;
+}
+
+- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    
+}
+
+#pragma mark - Override
+
+
+#pragma mark - Initial Methods
+
+
+#pragma mark - Target Methods
+
+
+#pragma mark - Notification Methods
+
+
+#pragma mark - KVO Methods
+
+
+#pragma mark - Delegate, DataSource
+
+
+#pragma mark - Privater Methods
+
+- (UIImage *)gaussBlurImage:(UIImage *)image blurLevel:(float)level {
+   
+    if (image == nil) {
+        return image;
+    }
+    
+    
+    CIImage *inputImage = [[CIImage alloc] initWithImage:image];
+    /*Gauss模糊*/
+    CIFilter *gaussBlurFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [gaussBlurFilter setValue:inputImage forKey:@"inputImage"];
+    [gaussBlurFilter setValue:[NSNumber numberWithFloat:level] forKey: @"inputRadius"];
+    CIImage *outPutImage = [gaussBlurFilter valueForKey:kCIOutputImageKey];
+
+    
+    CIContext *ctx = [CIContext contextWithOptions:nil];
+    
+    CGRect outputRect = [outPutImage extent]; // 白边明显
+    CGRect inputRect = [inputImage extent]; // 白边相对少窄一很多
+    CGRect rect = inputRect;
+    
+        
+    CGImageRef resultImage = [ctx createCGImage:outPutImage fromRect:rect];
+    UIImage *bluredImage = [UIImage imageWithCGImage:resultImage];
+    CGImageRelease(resultImage);
+    
+    return bluredImage;
+}
+
+// 添加通用模糊效果,Need Import Accelerate/Accelerate.h
+// image是图片，blur是模糊度
+- (UIImage *)blurryImage:(UIImage *)image withBlurLevel:(CGFloat)blur
+{
+    if (image==nil)
+    {
+        return nil;
+    }
+    //模糊度,
+    if (blur < 0.025f) {
+        blur = 0.025f;
+    } else if (blur > 1.0f) {
+        blur = 1.0f;
+    }
+    
+    //boxSize必须大于0
+    int boxSize = (int)(blur * 100);
+    boxSize -= (boxSize % 2) + 1;
+
+    //图像处理
+    CGImageRef img = image.CGImage;
+    
+    //图像缓存,输入缓存，输出缓存
+    vImage_Buffer inBuffer, outBuffer;
+    vImage_Error error;
+    //像素缓存
+    void *pixelBuffer;
+    
+    //数据源提供者，Defines an opaque type that supplies Quartz with data.
+    CGDataProviderRef inProvider = CGImageGetDataProvider(img);
+    // provider’s data.
+    CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
+    
+    //宽，高，字节/行，data
+    inBuffer.width = CGImageGetWidth(img);
+    inBuffer.height = CGImageGetHeight(img);
+    inBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    inBuffer.data = (void*)CFDataGetBytePtr(inBitmapData);
+    
+    //像数缓存，字节行*图片高
+    pixelBuffer = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img));
+    outBuffer.data = pixelBuffer;
+    outBuffer.width = CGImageGetWidth(img);
+    outBuffer.height = CGImageGetHeight(img);
+    outBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    // 第三个中间的缓存区,抗锯齿的效果
+    void *pixelBuffer2 = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img));
+    vImage_Buffer outBuffer2;
+    outBuffer2.data = pixelBuffer2;
+    outBuffer2.width = CGImageGetWidth(img);
+    outBuffer2.height = CGImageGetHeight(img);
+    outBuffer2.rowBytes = CGImageGetBytesPerRow(img);
+    //Convolves a region of interest within an ARGB8888 source image by an implicit M x N kernel that has the effect of a box filter.
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer2, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    error = vImageBoxConvolve_ARGB8888(&outBuffer2, &inBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    if (error) {
+        NSLog(@"error from convolution %ld", error);
+    }
+
+    //颜色空间DeviceRGB
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    //用图片创建上下文,CGImageGetBitsPerComponent(img),7,8
+    CGContextRef ctx = CGBitmapContextCreate(
+                                             outBuffer.data,
+                                             outBuffer.width,
+                                             outBuffer.height,
+                                             8,
+                                             outBuffer.rowBytes,
+                                             colorSpace,
+                                             CGImageGetBitmapInfo(image.CGImage));
+    
+    //根据上下文，处理过的图片，重新组件
+    CGImageRef imageRef = CGBitmapContextCreateImage (ctx);
+    UIImage *returnImage = [UIImage imageWithCGImage:imageRef];
+    //clean up
+    CGContextRelease(ctx);
+    CGColorSpaceRelease(colorSpace);
+    free(pixelBuffer);
+    free(pixelBuffer2);
+    CFRelease(inBitmapData);
+
+    CGImageRelease(imageRef);
+    return returnImage;
+}
+
+- (void)visualEffectView:(UIView *)view {
+    UIVisualEffectView *effectView = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleLight]];
+    effectView.frame = self.view.bounds;
+  
+    [view addSubview:effectView];
+}
+
+#pragma mark - Setter Getter Methods
+
 
 
 @end
